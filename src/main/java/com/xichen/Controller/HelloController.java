b5abaae265f3adc878191cff45baf5f334e14ace
XiChen1234 <xichen888.private@gmail.com>
2026-04-08 23:16:18 +0800

    test: add test post api

--- src/main/java/com/xichen/Controller/HelloController.java
+++ src/main/java/com/xichen/Controller/HelloController.java
@@ -5,6 +5,7 @@ import com.xichen.Entity.DO.Game;
 import com.xichen.Mapper.GameMapper;
 import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -24,5 +25,23 @@ public class HelloController {
     @GetMapping("/test/")
     public List<Game> get() {
         return gameMapper.selectList(new QueryWrapper<>());
+    }
+
+    @PostMapping("/test/")
+    public String post() {
+        Game game = new Game();
+        game.setGroupId(1L);
+        game.setName("test");
+        game.setIcon("https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png");
+        game.setDescription("test");
+        game.setMinPlayer(1);
+        game.setMaxPlayer(1);
+        game.setDuration(30);
+        game.setImages("[\"https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png\"]");
+        game.setStar(1);
+        game.setEnabled(true);
+
+        gameMapper.insert(game);
+        return "POST";
     }
 }
