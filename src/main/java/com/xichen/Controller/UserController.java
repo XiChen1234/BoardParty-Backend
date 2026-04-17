bb68b29743668d27f9cdc3c48f943317f13a6eaa
XiChen1234 <xichen888.private@gmail.com>
2026-04-17 08:56:07 +0800

    feat: add get user self api
    增加获取用户自身信息的接口

--- src/main/java/com/xichen/Controller/UserController.java
+++ src/main/java/com/xichen/Controller/UserController.java
@@ -2,9 +2,15 @@ package com.xichen.Controller;
 
 import com.xichen.Common.CommonResponse;
 import com.xichen.Entity.Response.UserResponse;
+import com.xichen.Security.JwtUtil;
 import com.xichen.Service.UserService;
 import jakarta.annotation.Resource;
-import org.springframework.web.bind.annotation.*;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 用户模块
@@ -14,9 +20,20 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
     @Resource
     private UserService userService;
+    @Resource
+    private JwtUtil jwtUtil;
+
     @GetMapping("/{id}")
     public CommonResponse<UserResponse> getUser(@PathVariable Long id) {
         UserResponse userResponse = userService.getUserInfo(id);
+        return CommonResponse.success(userResponse);
+    }
+
+    @GetMapping("/me")
+    public CommonResponse<UserResponse> getUserSelf(HttpServletRequest request) {
+        // 获取token，从token中获取用户id
+        Long uid = (Long) request.getAttribute("uid");
+        UserResponse userResponse = userService.getUserInfo(uid);
         return CommonResponse.success(userResponse);
     }
 }
